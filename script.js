6ce77645c4519d2754e090101869e1d8fa906343
williamnym13 <e-wilnym@iskule.no>
2025-04-30 14:54:42 +0100

    Portet over Cookie Clicker klonen til nettsiden min, så langt fungerende!

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ document.addEventListener('keydown', function (event) {
         event.preventDefault();
     }
 });
+
+let cookieCounter = document.querySelector('#cookie-amount');
+let currentCookieAmount = 0;
+let cookieMultiplier = 1;
+let upgradeValue = 1;
+const cookieImage = document.querySelector("#cookieImage");
+cookieCounter.innerHTML = currentCookieAmount;
+
+function cookie_click () {
+    if (upgradeValue == 1) {
+        currentCookieAmount = currentCookieAmount + 1 * cookieMultiplier;
+        cookieCounter.innerHTML = currentCookieAmount;
+        console.log(currentCookieAmount);
+    }
+}
+function Upgrader () {
+    if (upgradeValue == 1) {
+        if (currentCookieAmount >= 50) {
+            currentCookieAmount = 0;
+            cookieCounter.innerHTML = currentCookieAmount;
+            upgradeValue = 2;
+            console.log(upgradeValue);
+        }
+    }
+}
+
+document.querySelector('#cookieImage').addEventListener('click', cookie_click);
+document.querySelector('#upgradeOne').addEventListener('click', Upgrader);
